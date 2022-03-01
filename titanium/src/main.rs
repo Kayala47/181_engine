2478c12039b1be75e1c8efa0306510a0fc944e4c
Kevin Ayala <ayala.kevin99@gmail.com>
2022-03-01 14:42:51 -0800

    tried to merge evan's branch; not working

--- titanium/src/main.rs
+++ titanium/src/main.rs
@@ -1,6 +1,6 @@
 use engine::{
-    clear, draw, handle_winit_event, load_cards_from_file, setup,
-    Color, Drawable, DraggableSnapType, Event, Rect, VirtualKeyCode, check_and_handle_drag, generate_deck_slots
+    check_and_handle_drag, clear, draw, generate_deck_slots, handle_winit_event,
+    load_cards_from_file, setup, Color, DraggableSnapType, Drawable, Event, Rect, VirtualKeyCode,
 };
 use winit::event_loop::EventLoop;
 
@@ -8,7 +8,6 @@ const BACKGROUND_COLOR: Color = (91, 99, 112, 255);
 
 struct GameState {
     dragged: String,
-    
 }
 
 fn main() {
@@ -32,39 +31,38 @@ fn main() {
     let event_loop = EventLoop::new();
 
     let mut starting_game_objects: Vec<Drawable> = vec![];
-    
-    let mut boxes = vec![Drawable::Rectangle(r1, c1, Some(DraggableSnapType::Card(true, false))), Drawable::RectOutlined(r2, c2, Some(DraggableSnapType::Card(true, false)))];
-    
-    let mut slots = generate_deck_slots((30, 40), 5, 5, 5, (255,30,255,255),(0,255,0,255),(255,255,255,255),(220,220,250,255));
+
+    let mut boxes = vec![
+        Drawable::Rectangle(r1, c1, Some(DraggableSnapType::Card(true, false))),
+        Drawable::RectOutlined(r2, c2, Some(DraggableSnapType::Card(true, false))),
+    ];
+
+    let mut slots = generate_deck_slots(
+        (30, 40),
+        5,
+        5,
+        5,
+        (255, 30, 255, 255),
+        (0, 255, 0, 255),
+        (255, 255, 255, 255),
+        (220, 220, 250, 255),
+    );
 
     starting_game_objects.append(&mut slots);
     starting_game_objects.append(&mut boxes);
 
     state.drawables = starting_game_objects.clone();
     event_loop.run(move |event, _, control_flow| {
-
         if event == Event::MainEventsCleared {
             state.bg_color = BACKGROUND_COLOR;
             // let mut new_objects = game_objects.clone();
 
             check_and_handle_drag(&mut state);
             draw(&mut state);
-            
-            
-            let deck = load_cards_from_file("../cards2.json");
-
-            let mut game_objects: Vec<Drawable> = vec![];
-            game_objects.push(Drawable::Rectangle(r1, c1));
-            game_objects.push(Drawable::RectOutlined(r2, c2));
-
-            draw(&mut state, game_objects);
 
             let deck = load_cards_from_file("../cards2.json");
-
         }
 
         handle_winit_event(event, control_flow, &mut state);
     });
 }
-
-
