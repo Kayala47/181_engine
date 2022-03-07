815c8bfd1d22f7fef472ebcc659a332e64b42d9f
Kevin Ayala <ayala.kevin99@gmail.com>
2022-03-07 00:50:40 -0800

    made playedcard draggable

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -141,7 +141,7 @@ impl PlayedCard {
             self.rect,
             self.card.get_description(),
             CARD_COLOR,
-            Some(DraggableSnapType::PlayedCard(false, true)),
+            Some(DraggableSnapType::PlayedCard(true, false)),
         )
     }
 }
